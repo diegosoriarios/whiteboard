5f7dce39934798877bbc02f4a47484557cdfa98f
diegosoriarios <diegosoriarios@gmail.com>
2019-08-08 21:03:32 -0300

    first commit

---
+++ src/app/whiteboard-page/whiteboard-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WhiteboardPageComponent } from './whiteboard-page.component';
+
+describe('WhiteboardPageComponent', () => {
+  let component: WhiteboardPageComponent;
+  let fixture: ComponentFixture<WhiteboardPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WhiteboardPageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WhiteboardPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
